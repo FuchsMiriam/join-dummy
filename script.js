e6e66fd4ec3a22a0694361f1b52fe51fa888e1ee
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-05-23 16:38:12 +0200

    added animation

---
+++ script.js
@@ -0,0 +1,22 @@
+// Funktion für die Animation auf der Login-Seite
+
+window.onload = function() {
+    const logo = document.getElementById('animatedLogo');
+    const content = document.getElementById('contentMainpage');
+
+    setTimeout(() => {
+        logo.style.width = '100.03px';
+        logo.style.height = '121.97px';
+        logo.style.top = '80px';
+        logo.style.left = '77px';
+    }, 1000);
+
+    function onTransitionEnd() {
+        logo.removeEventListener('transitionend', onTransitionEnd);
+
+        content.classList.remove('hiddenMainpage');
+        content.classList.add('visibleMainpage');
+    }
+
+    logo.addEventListener('transitionend', onTransitionEnd);
+};
