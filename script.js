6d84a549449cd1ae1ceb6b9eeae8b2cad5a32b67
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-05-24 19:23:08 +0200

    changed animation

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 // Funktion für die Animation auf der Login-Seite
 
-window.onload = function() {
-    const logo = document.getElementById('animatedLogo');
-    const content = document.getElementById('contentMainpage');
+window.onload = function () {
+  const logo = document.getElementById("animatedLogo");
+  const content = document.getElementById("contentMainpage");
 
-    setTimeout(() => {
-        logo.style.width = '100.03px';
-        logo.style.height = '121.97px';
-        logo.style.top = '80px';
-        logo.style.left = '77px';
-    }, 500);
+  setTimeout(() => {
+    logo.style.width = "100.03px";
+    logo.style.height = "121.97px";
+    logo.style.top = "80px";
+    logo.style.left = "77px";
+  }, 500);
 
-    function onTransitionEnd() {
-        logo.removeEventListener('transitionend', onTransitionEnd);
+  function onTransitionEnd() {
+    logo.removeEventListener("transitionend", onTransitionEnd);
+    content.classList.remove("hiddenMainpage");
+    content.classList.add("visibleMainpage");
+  }
 
-        content.classList.remove('hiddenMainpage');
-        /*content.classList.add('visibleMainpage');*/
-    }
-
-    logo.addEventListener('transitionend', onTransitionEnd);
+  logo.addEventListener("transitionend", onTransitionEnd);
 };
 
 // Funktion zur Weiterleitung auf die Sign-up-Seite
 
 function redirectToSignUpPage() {
-    window.location.href = './signup.html';
-  }
+  window.location.href = "./signup.html";
+}
