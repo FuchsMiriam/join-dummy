16b31253ceb32fc545ebfcaeddaf262835e342b5
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-06-14 12:08:08 +0200

    media query js

--- js/contacts_mediaqueries.js
+++ js/contacts_mediaqueries.js
@@ -1,13 +1,22 @@
 /*Change image media query dot icon*/
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const dotIcon = document.getElementById('dotIcon');
-  
-    dotIcon.addEventListener('mouseover', () => {
-        dotIcon.src = '../assets/img/dotIconBlue.svg';
-    });
-  
-    dotIcon.addEventListener('mouseout', () => {
-        dotIcon.src = '../assets/img/dotIcon.svg';
+document.addEventListener("DOMContentLoaded", (event) => {
+  const dotIcon = document.getElementById("dotIcon");
+
+  dotIcon.addEventListener("mouseover", () => {
+    dotIcon.src = "../assets/img/dotIconBlue.svg";
+  });
+
+  dotIcon.addEventListener("mouseout", () => {
+    dotIcon.src = "../assets/img/dotIcon.svg";
+  });
+});
+
+/*Close small Add contact overlay*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("closeAddOverlay").addEventListener("click", function () {
+        document.querySelector(".addNewContactOverlay").classList.add("hidden");
+        document.querySelector(".addNewContactOverlay").classList.remove("visible");
     });
   });
