234534722fb5cf3101d3f7f9c0f553e4075d6cb0
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-07-17 12:48:50 +0200

    JSDoc signup page

--- js/signup.js
+++ js/signup.js
@@ -1,34 +1,50 @@
-let contactBase = [];
-//Function to redirect to the Privacy Policy page
+const databaseURL =
+  "https://users-f61ab-default-rtdb.europe-west1.firebasedatabase.app/";
 
+let contactBase = [];
+
+/**
+ * Redirects the user to the privacy policy page.
+ */
 function redirectToPrivacyPage() {
   window.location.href = "./privacy_policy.html";
 }
 
-//Function to redirect to the Legal Notice page
+/**
+ * Redirects the user to the legal notice page.
+ */
 function redirectToLegalPage() {
   window.location.href = "./legal_notice.html";
 }
 
-// Function to redirect to the Login page
+/**
+ * Redirects the user to the login page.
+ */
 function redirectToLoginPage() {
   window.location.href = "../index.html";
 }
 
 // Sign up button
 
+/**
+ * Adds event listeners to set up signup button functionality and checkbox toggle on DOM content loaded.
+ */
 document.addEventListener("DOMContentLoaded", () => {
   buttonDisabled();
   checkboxToggle();
 });
 
+/**
+ * Disables the signup button initially.
+ */
 function buttonDisabled() {
   const signupButton = document.querySelector("#signupButton");
   signupButton.disabled = true;
 }
 
-//Checkbox toggle functionality
-
+/**
+ * Toggles the signup button enabled/disabled state based on the 'rememberMe' checkbox state.
+ */
 function checkboxToggle() {
   const checkbox = document.querySelector("#rememberMe");
   const signupButton = document.querySelector("#signupButton");
@@ -38,8 +54,9 @@ function checkboxToggle() {
   });
 }
 
-//Functions for toggling password visibility - icons and text input
-
+/**
+ * Sets up password visibility toggle functionality for password input fields on DOM content loaded.
+ */
 document.addEventListener("DOMContentLoaded", () => {
   setupPasswordVisibilityToggle("signupPasswordInput", "signupLockIcon");
   setupPasswordVisibilityToggle(
@@ -48,7 +65,12 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 });
 
-// Function to set up password visibility toggle
+/**
+ * 
+ * Sets up password visibility toggle for a given password input and lock icon.
+ * @param {string} passwordInputId - The ID of the password input element.
+ * @param {string} lockIconId - The ID of the lock icon element.
+ */
 function setupPasswordVisibilityToggle(passwordInputId, lockIconId) {
   const passwordInput = document.getElementById(passwordInputId);
   const lockIcon = document.getElementById(lockIconId);
@@ -61,7 +83,12 @@ function setupPasswordVisibilityToggle(passwordInputId, lockIconId) {
   );
 }
 
-// Function to update lock icon based on input
+/**
+ * 
+ * Updates the lock icon based on the password input length.
+ * @param {HTMLElement} passwordInput - The password input element.
+ * @param {HTMLElement} lockIcon - The lock icon element.
+ */
 function updateLockIcon(passwordInput, lockIcon) {
   lockIcon.src =
     passwordInput.value.length > 0
@@ -69,7 +96,12 @@ function updateLockIcon(passwordInput, lockIcon) {
       : "../assets/img/lockIcon.png";
 }
 
-// Function to toggle password visibility
+/**
+ * 
+ * Toggles the visibility of the password input.
+ * @param {HTMLElement} passwordInput - The password input element.
+ * @param {HTMLElement} lockIcon - The lock icon element.
+ */
 function togglePasswordVisibility(passwordInput, lockIcon) {
   const passwordVisible = passwordInput.type === "password";
   lockIcon.src = passwordVisible
@@ -78,11 +110,12 @@ function togglePasswordVisibility(passwordInput, lockIcon) {
   passwordInput.type = passwordVisible ? "text" : "password";
 }
 
-//Database for signup
-
-const databaseURL =
-  "https://users-f61ab-default-rtdb.europe-west1.firebasedatabase.app/";
-
+/**
+ * 
+ * Loads data from the specified path in the Firebase Realtime Database.
+ * @param {string} [path=""] - The path to load data from.
+ * @returns {Promise<object>} A promise that resolves to the JSON response from the database.
+ */
 async function onloadDatabase(path = "") {
   let response = await fetch(databaseURL + path + ".json");
   let responseToJson = await response.json();
@@ -90,8 +123,14 @@ async function onloadDatabase(path = "") {
   return responseToJson;
 }
 
-// Function to post data to the Firebase database
-
+/**
+ * 
+ * Posts data to the specified path in the Firebase Realtime Database.
+ * @param {string} [path=""] - The path to post data to.
+ * @param {object} [data={}] - The data to post.
+ * @returns {Promise<object>} A promise that resolves to the JSON response from the database.
+ * @throws Will throw an error if the data posting fails.
+ */
 async function postData(path = "", data = {}) {
   try {
     let response = await fetch(`${databaseURL}${path}.json`, {
@@ -107,8 +146,11 @@ async function postData(path = "", data = {}) {
   }
 }
 
-//Function for signup
-
+/**
+ * 
+ * Handles the signup process by validating the form, resetting error messages, 
+ * checking email existence, creating a new user, and posting the user data to the database.
+ */
 async function signup() {
   const name = document.getElementById("signupNameInput").value;
   const email = document.getElementById("signupEmailInput").value;
@@ -128,7 +170,11 @@ async function signup() {
   }
 }
 
-// Function to display custom error message
+/**
+ * 
+ * Displays a custom error message temporarily.
+ * @param {string} message - The custom error message to display.
+ */
 function displayCustomErrorMessage(message) {
   const customErrorMessage = document.getElementById("customErrorMessage");
   customErrorMessage.textContent = message;
@@ -138,6 +184,13 @@ function displayCustomErrorMessage(message) {
   }, 3000);
 }
 
+/**
+ * 
+ * Handles email check and user creation process.
+ * @param {string} name - The name of the user.
+ * @param {string} email - The email of the user.
+ * @param {string} password - The password of the user.
+ */
 async function handleEmailCheckAndUserCreation(name, email, password) {
   const emailExists = await checkEmailExists(email);
   if (emailExists) {
@@ -155,7 +208,15 @@ async function handleEmailCheckAndUserCreation(name, email, password) {
   }
 }
 
-// Function to validate the signup form
+/**
+ * 
+ * Validates the signup form by checking field completeness, email validity, and password match.
+ * @param {string} name - The name of the user.
+ * @param {string} email - The email of the user.
+ * @param {string} password - The password of the user.
+ * @param {string} confirmPassword - The password confirmation.
+ * @returns {boolean} True if the form is valid, otherwise false.
+ */
 function validateSignupForm(name, email, password, confirmPassword) {
   if (!areFieldsFilled(name, email, password, confirmPassword)) {
     displayCustomErrorMessage("Please fill in all fields.");
@@ -175,34 +236,68 @@ function validateSignupForm(name, email, password, confirmPassword) {
   return true;
 }
 
-// Function to check if all fields are filled
+/**
+ * 
+ * Checks if all signup form fields are filled.
+ * @param {string} name - The name of the user.
+ * @param {string} email - The email of the user.
+ * @param {string} password - The password of the user.
+ * @param {string} confirmPassword - The password confirmation.
+ * @returns {boolean} True if all fields are filled, otherwise false.
+ */
 function areFieldsFilled(name, email, password, confirmPassword) {
   return name && email && password && confirmPassword;
 }
 
-// Function to validate the email pattern
+/**
+ * 
+ * Validates the email format using a regular expression.
+ * @param {string} email - The email to validate.
+ * @returns {boolean} True if the email is valid, otherwise false.
+ */
 function isEmailValid(email) {
   const emailPattern =
     /^[^\s@]+@[^\s@]+\.(com|de|org|net|edu|gov|mil|info|io|co)$/;
   return emailPattern.test(email);
 }
 
-// Function to check if passwords match
+/**
+ * 
+ * Checks if the password and confirm password fields match.
+ * @param {string} password - The password of the user.
+ * @param {string} confirmPassword - The password confirmation.
+ * @returns {boolean} True if the passwords match, otherwise false.
+ */
 function arePasswordsMatching(password, confirmPassword) {
   return password === confirmPassword;
 }
 
-// Function to show error message for password mismatch
+/**
+ * 
+ * Shows an error message for password mismatch and adds error styling to the confirm password field.
+ */
 function showError() {
   document.getElementById("errorMessage").style.display = "block";
   document.getElementById("signupConfirmPassword").classList.add("error");
 }
 
+/**
+ * 
+ * Resets error messages and removes error styling from the confirm password field.
+ */
 function resetErrorMessages() {
   document.getElementById("errorMessage").style.display = "none";
   document.getElementById("signupConfirmPassword").classList.remove("error");
 }
 
+/**
+ * 
+ * Creates a new user object with the given name, email, and password.
+ * @param {string} name - The name of the user.
+ * @param {string} email - The email of the user.
+ * @param {string} password - The password of the user.
+ * @returns {object} The new user object.
+ */
 function createNewUser(name, email, password) {
   return {
     name: name,
@@ -212,6 +307,10 @@ function createNewUser(name, email, password) {
   };
 }
 
+/**
+ * 
+ * Handles successful signup by showing an overlay and redirecting to the login page.
+ */
 function handleSuccessfulSignup() {
   const overlay = document.getElementById("overlay");
   overlay.classList.add("show");
@@ -221,13 +320,23 @@ function handleSuccessfulSignup() {
   }, 2000);
 }
 
+/**
+ * 
+ * Handles signup error by logging the error and displaying an alert with the error message.
+ * @param {Error} error - The error that occurred during signup.
+ */
 function handleSignupError(error) {
   console.error("Error creating user:", error.message);
   alert("Error creating user: " + error.message);
 }
 
-//Check if email address exists
-
+/**
+ * 
+ * Checks if an email address already exists in the database.
+ * @param {string} email - The email address to check.
+ * @returns {Promise<boolean>} A promise that resolves to true if the email exists, otherwise false.
+ * @throws Will throw an error if the email check fails.
+ */
 async function checkEmailExists(email) {
   try {
     const response = await fetch(`${databaseURL}users.json`);
